553adc51ce19d6d7435d3d2d92224fc59e6f1c0b
Rafael Delgado <rafael.santos@codewin.pt>
2024-03-17 16:40:43 +0000

    Created Business Services

---
+++ bookstoreChallenge.business/Services/Book/IBookService.cs
@@ -0,0 +1,10 @@
+﻿using BookModel = bookstoreChallenge.business.Models.Book;
+
+namespace bookstoreChallenge.business.Services.Book
+{
+    public interface IBookService
+    {
+        Task<BookModel.Book> GetById(Guid id);
+        Task<IEnumerable<BookModel.Book>> GetAll();
+    }
+}
---
+++ bookstoreChallenge.business/Services/File/IFIleService.cs
@@ -0,0 +1,9 @@
+﻿using bookstoreChallenge.business.Models.File;
+
+namespace bookstoreChallenge.business.Services.File
+{
+    public interface IFIleService
+    {
+        Task<ImageFile> GetFileById(Guid id);
+    }
+}
