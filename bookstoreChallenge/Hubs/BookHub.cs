9bdfa01d4bc4a11e338fb3ce0e1d6f4f292d0056
Rafael Delgado <rafael.santos@codewin.pt>
2024-03-17 16:39:45 +0000

    Added Hub to application

---
+++ bookstoreChallenge/Hubs/BookHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace bookstoreChallenge.app.Hubs
+{
+    public class BookHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            var response = $"Client ID: {Context.ConnectionId} conected";
+            await Clients.Caller.SendAsync(response);
+        }
+    }
+}
--- bookstoreChallenge/Program.cs
+++ bookstoreChallenge/Program.cs
@@ -1,7 +1,9 @@
+using bookstoreChallenge.app.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSignalR().AddJsonProtocol();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,10 +18,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseEndpoints(endpoints => {
+    endpoints.MapControllers();
+    endpoints.MapHub<BookHub>("/books");
+});
+
+app.UseHttpsRedirection();
+
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
