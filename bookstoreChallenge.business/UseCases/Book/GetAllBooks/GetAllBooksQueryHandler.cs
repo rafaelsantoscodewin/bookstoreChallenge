334f978e1a8e2d44880a664f55d1d53868967e1c
Rafael Delgado <rafael.santos@codewin.pt>
2024-03-18 09:25:06 +0000

    Updated UseCases to use database

--- bookstoreChallenge.business/UseCases/Book/GetAllBooks/GetAllBooksQueryHandler.cs
+++ bookstoreChallenge.business/UseCases/Book/GetAllBooks/GetAllBooksQueryHandler.cs
@@ -1,4 +1,4 @@
-﻿using bookstoreChallenge.business.Models.File;
+﻿using bookstoreChallenge.business.Services.Book;
 using MediatR;
 using BookModel = bookstoreChallenge.business.Models.Book;
 
@@ -6,60 +6,62 @@ namespace bookstoreChallenge.business.UseCases.Book.GetAllBooks
 {
     public class GetAllBooksQueryHandler : IRequestHandler<GetAllBooksQuery, List<BookModel.Book>>
     {
-        //private readonly IBookService _bookService;
+        private readonly IBookService _bookService;
 
-        //public GetAllBooksQueryHandler(IBookService bookService)
-        //{
-        //    _bookService = bookService ?? throw new ArgumentNullException(nameof(bookService));
-        //}
-
-        public Task<List<BookModel.Book>> Handle(GetAllBooksQuery request, CancellationToken cancellationToken)
+        public GetAllBooksQueryHandler(IBookService bookService)
         {
-            var book1 = new BookModel.Book
-            {
-                Id = Guid.NewGuid(),
-                Title = "Title Teste 1",
-                Author = "Name Surname",
-                Publisher = "Publisher Test",
-                ISBN = "123456-789101112-13141516-1718192021",
-                Image = new ImageFile(),
-                Price = 24.55d,
-                Status = true,
-                Genre = "Fantasy"
+            _bookService = bookService ?? throw new ArgumentNullException(nameof(bookService));
+        }
 
-            };
+        public async Task<List<BookModel.Book>> Handle(GetAllBooksQuery request, CancellationToken cancellationToken)
+        {
+            //var book1 = new BookModel.Book
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "Title Teste 1",
+            //    Author = "Name Surname",
+            //    Publisher = "Publisher Test",
+            //    ISBN = "123456-789101112-13141516-1718192021",
+            //    Image = new ImageFile(),
+            //    Price = 24.55d,
+            //    Status = true,
+            //    Genre = "Fantasy"
 
-            var book2 = new BookModel.Book
-            {
-                Id = Guid.NewGuid(),
-                Title = "Title Teste 2",
-                Author = "Name Surname",
-                Publisher = "Publisher Test",
-                ISBN = "123456-789101112-13141516-1718192021",
-                Image = new ImageFile(),
-                Price = 24.55d,
-                Status = true,
-                Genre = "Drama"
+            //};
 
-            };
+            //var book2 = new BookModel.Book
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "Title Teste 2",
+            //    Author = "Name Surname",
+            //    Publisher = "Publisher Test",
+            //    ISBN = "123456-789101112-13141516-1718192021",
+            //    Image = new ImageFile(),
+            //    Price = 24.55d,
+            //    Status = true,
+            //    Genre = "Drama"
 
-            var book3 = new BookModel.Book
-            {
-                Id = Guid.NewGuid(),
-                Title = "Title Teste 3",
-                Author = "Name Surname",
-                Publisher = "Publisher Test",
-                ISBN = "123456-789101112-13141516-1718192021",
-                Image = new ImageFile(),
-                Price = 24.55d,
-                Status = false,
-                Genre = "Adventure"
+            //};
 
-            };
+            //var book3 = new BookModel.Book
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "Title Teste 3",
+            //    Author = "Name Surname",
+            //    Publisher = "Publisher Test",
+            //    ISBN = "123456-789101112-13141516-1718192021",
+            //    Image = new ImageFile(),
+            //    Price = 24.55d,
+            //    Status = false,
+            //    Genre = "Adventure"
 
-            var response = new List<BookModel.Book> { book1, book2, book3 };
+            //};
 
-            return Task.FromResult(response);
+            //var response = new List<BookModel.Book> { book1, book2, book3 };
+
+            var response = await _bookService.GetAll();
+
+            return response.ToList();
         }
     }
 }
--- bookstoreChallenge.business/UseCases/Book/GetBookById/GetBookByIdQueryHandler.cs
+++ bookstoreChallenge.business/UseCases/Book/GetBookById/GetBookByIdQueryHandler.cs
@@ -1,4 +1,4 @@
-﻿using bookstoreChallenge.business.Models.File;
+﻿using bookstoreChallenge.business.Services.Book;
 using MediatR;
 using BookModel = bookstoreChallenge.business.Models.Book;
 
@@ -6,31 +6,32 @@ namespace bookstoreChallenge.business.UseCases.Book.GetBookById
 {
     public class GetBookByIdQueryHandler : IRequestHandler<GetBookByIdQuery, BookModel.Book>
     {
-        //private readonly IBookService _bookService;
+        private readonly IBookService _bookService;
         //private readonly IFIleService _fileService;
 
-        //public GetBookByIdQueryHandler(IBookService bookService, IFIleService fileService)
-        //{
-        //    _bookService = bookService ?? throw new ArgumentNullException(nameof(bookService));
-        //    _fileService = fileService ?? throw new ArgumentNullException(nameof(fileService));
-        //}
-
-        public Task<BookModel.Book> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
+        public GetBookByIdQueryHandler(IBookService bookService/*, IFIleService fileService*/)
         {
-            var book = new BookModel.Book
-            {
-                Id = request.Id,
-                Title = "Title Teste",
-                Author = "Name Surname",
-                Publisher = "Publisher Test",
-                ISBN = "123456-789101112-13141516-1718192021",
-                Image = new ImageFile { Id = request.Id, Name = "Image", Extension = "jpeg", Data = new byte[] { 1, 2, 3, 4, 5 } },
-                Price = 24.55d,
-                Status = true,
-                Genre = "Fiction"
-            };
+            _bookService = bookService ?? throw new ArgumentNullException(nameof(bookService));
+            //_fileService = fileService ?? throw new ArgumentNullException(nameof(fileService));
+        }
 
-            return Task.FromResult(book);
+        public async Task<BookModel.Book> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
+        {
+            //var book = new BookModel.Book
+            //{
+            //    Id = request.Id,
+            //    Title = "Title Teste",
+            //    Author = "Name Surname",
+            //    Publisher = "Publisher Test",
+            //    ISBN = "123456-789101112-13141516-1718192021",
+            //    Image = new ImageFile { Id = request.Id, Name = "Image", Extension = "jpeg", Data = new byte[] { 1, 2, 3, 4, 5 } },
+            //    Price = 24.55d,
+            //    Status = true,
+            //    Genre = "Fiction"
+            //};
+
+            var response = await _bookService.GetById(request.Id);
+            return response;
         }
     }
 }
